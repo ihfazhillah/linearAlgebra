1f07b4e50b9b44bd427c8151cb9273950399f100
ihfazhillah <mihfazhillah@gmail.com>
2017-10-16 21:45:44 +0700

    add dot product and angle (radians and degree)

--- index.js
+++ index.js
@@ -33,17 +33,37 @@ Vector.prototype.timesScalar = function(other) {
   return new Vector(_.map(this.coordinate, (me) => (me*other)))
 }
 
-Vector.prototype.magnitude = function() {
+Vector.prototype.normalized = function() {
   return Math.sqrt(_.sum(_.map(this.coordinate, (x) => Math.pow(x, 2))))
 }
 
-Vector.prototype.normalize = function() {
-  let magnitude = this.magnitude()
+Vector.prototype.magnitude = function() {
+  let magnitude = this.normalized()
 
   if (magnitude === 0){
     throw "denominator is zero"
   }
   return this.timesScalar(1./magnitude)
+}
+
+Vector.prototype.dot = function(other) {
+  let zipped = _.zipWith(this.coordinate, other.coordinate, (me, other) => (me * other))
+  return _.sum(zipped)
+}
+
+Vector.prototype.angle = function(other, inDegrees) {
+  let normMe = this.normalized()
+  let normOther = other.normalized()
+  
+  let dotted = this.dot(other)
+
+  let angleInRad = Math.acos(dotted / (normMe * normOther))
+
+  if(inDegrees){
+    return angleInRad * 180 / Math.PI
+  } 
+
+  return angleInRad
 }
 
 
